48c271c7bb465cb4acb59614ebc5c701b5c8c225
Vlad Frangu <kingdgrizzle@gmail.com>
2020-10-14 23:49:14 +0300

    chore: initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Default ESLint configuration for oldschoolgg Projects
+ * @example
+ * ```json
+ * {
+ *   "extends": "@oldschoolgg/config"
+ * }
+ * ```
+ */
+export const config = {
+	extends: ['@sapphire']
+};
+
+module.exports = config;
+export default config;
